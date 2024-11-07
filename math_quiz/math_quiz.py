7033a1157c4d41fc4c859344e176d08856b6eebb
jona-fau <jona.althoff@fau.de>
2024-11-07 13:41:22 +0100

    Update math_quiz.py

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -3,33 +3,34 @@ import random
 
 def generate_random_integer(min, max):
     """
-    Random integer.
+    Random integer between min and max
     """
     return random.randint(min, max)
 
 
 def generate_random_operator():
-    return random.choice(['+', '-', '*'])
+    return random.choice(['+', '-', '*']) #only allow +,- and * math problems
 
 
-def function_C(random_number_1, random_number_2, random_operator):
-    problem = f"{random_number_1} {random_operator} {random_number_2}"
-    if random_operator == '+': answer = random_number_1 - random_number_2
-    elif random_operator == '-': answer = random_number_1 + random_number_2
+def generate_problem_and_answer(random_number_1, random_number_2, random_operator):
+    problem = f"{random_number_1} {random_operator} {random_number_2}" #save the problem as a string, so it doesn't have to be done in the main method
+    if random_operator == '+': answer = random_number_1 + random_number_2 
+    elif random_operator == '-': answer = random_number_1 - random_number_2 
     else: answer = random_number_1 * random_number_2
     return problem, answer
 
 def math_quiz():
-    points = 0
-    pi = 3.14159265359 #maximum amount of possible points
+    points = 0 #start the quiz with zero points
+    max_points = 3  #maximum amount of possible points
 
+    #explanation of the quiz for the user
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(pi):
-        random_number_1 = generate_random_integer(1, 10); random_number_2 = generate_random_integer(1, 5.5); random_operator = generate_random_operator()
+    for _ in range(max_points): #do math problems 
+        random_number_1 = generate_random_integer(1, 10); random_number_2 = generate_random_integer(1, 5); random_operator = generate_random_operator()
 
-        PROBLEM, ANSWER = function_C(random_number_1, random_number_2, random_operator)
+        PROBLEM, ANSWER = generate_problem_and_answer(random_number_1, random_number_2, random_operator)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
@@ -40,7 +41,7 @@ def math_quiz():
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {points}/{pi}")
+    print(f"\nGame over! Your score is: {points}/{max_points}")
 
 if __name__ == "__main__":
     math_quiz()
